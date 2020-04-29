c587c40121778629ee32ed777ce2035ee8537311
Hussein Kaplan <30393730+hussein-kaplan@users.noreply.github.com>
2020-04-29 22:11:15 +0300

    Update kaplan.sh

--- kaplan.sh
+++ kaplan.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # nehir
-# coded by:  hussein kaplan
+# coded by:  hussein kaplan :)
 
 
 string4=$(openssl rand -hex 32 | cut -c 1-4)
@@ -20,18 +20,24 @@ ig_sig="4f8732eb9ba7d1c8e8897a75d6474d4eb3f5279137431b2aafb71fafe2abe178"
 
 banner() {
 printf " \n"
-printf " \e[1;31mM\"\"M\e[0m\e[1;77m                   \e[0m\e[1;93mdP                         dP       \e[0m\e[1;77mdP           \e[0m\n"
-printf " \e[1;31mM  M\e[0m\e[1;77m                   \e[0m\e[1;93m88                         88       \e[0m\e[1;77m88           \e[0m\n"
-printf " \e[1;31mM  M\e[0m\e[1;77m 88d888b. .d8888b. \e[0m\e[1;93m88d888b. .d8888b. .d8888b. 88  .dP  \e[0m\e[1;77m88 .d8888b.  \e[0m\n"
-printf " \e[1;31mM  M\e[0m\e[1;77m 88'  \`88 Y8ooooo. \e[0m\e[1;93m88\'  \`88 88\'  \`88 88'  \`\"\" 88888\"   \e[0m\e[1;77m88 88ooood8  \e[0m\e[0m\n"
-printf " \e[1;31mM  M\e[0m\e[1;77m 88    88       88 \e[0m\e[1;93m88    88 88.  .88 88.  ... 88  \`8b. \e[0m\e[1;77m88 88.  ...  \e[0m\n"
-printf " \e[1;31mM  M\e[0m\e[1;77m dP    dP \`88888P\' \e[0m\e[1;93mdP    dP \`88888P8 \`88888P\' dP   \`YP \e[0m\e[1;77mdP \`88888P\'  \e[0m\n"
-printf " \e[1;31mMMMM\e[0m                                                                    \n"
 
+
+                                                             
+printf " \e[1;77m88                                  88                           \e[0m\n"
+printf " \e[1;77m88                                  88                           \e[0m\n"
+printf " \e[1;77m88                                  88                           \e[0m\n"
+printf " \e[1;77m88   ,d8   ,adPPYYba,  8b,dPPYba,   88  ,adPPYYba,  8b,dPPYba,   \e[0m\n"
+printf " \e[1;77m88 ,a8"    ""     `Y8  88P'    "8a  88  ""     `Y8  88P'   `"8a  \e[0m\n"
+printf " \e[1;77m8888[      ,adPPPPP88  88       d8  88  ,adPPPPP88  88       88  \e[0m\n"
+printf " \e[1;77m88`"Yba,   88,    ,88  88b,   ,a8"  88  88,    ,88  88       88  \e[0m\n"
+printf " \e[1;77m88   `Y8a  `"8bbdP"Y8  88`YbbdP"'   88  `"8bbdP"Y8  88       88  \e[0m\n"
+printf " \e[1;77m                       88                                        \e[0m\n"
+printf " \e[1;77m                       88                                        \e[0m\n"
 
 
 printf "\n"
-printf "https://www.facebook.com/husseinkaplan17"
+
+printf "    https://www.facebook.com/husseinkaplan17"
 }
 
 
@@ -40,13 +46,13 @@ login_user() {
 
 
 if [[ $user == "" ]]; then
-printf "\e[1;31m[\e[0m\e[1;77m*\e[0m\e[1;31m]\e[0m\e[1;93m Login\e[0m\n"
+printf "\e[1;31m[\e[0m\e[1;77m*\e[0m\e[1;31m]\e[0m\e[1;93m You need login first\e[0m\n"
 read -p $'\e[1;31m[\e[0m\e[1;77m+\e[0m\e[1;31m]\e[0m\e[1;93m Username: \e[0m' user
 fi
 
 if [[ -e cookie.$user ]]; then
 
-printf "\e[1;31m[\e[0m\e[1;77m*\e[0m\e[1;31m]\e[0m\e[1;93m Cookies found for user\e[0m\e[1;77m %s\e[0m\n" $user
+printf "\e[1;31m[\e[0m\e[1;77m❥\e[0m\e[1;31m]\e[0m\e[1;93m informition  found for user\e[0m\e[1;77m %s\e[0m\n" $user
 
 default_use_cookie="Y"
 
@@ -55,7 +61,7 @@ read -p $'\e[1;31m[\e[0m\e[1;77m+\e[0m\e[1;31m]\e[0m\e[1;93m Use it?\e[0m\e[1;77
 use_cookie="${use_cookie:-${default_use_cookie}}"
 
 if [[ $use_cookie == *'Y'* || $use_cookie == *'y'* ]]; then
-printf "\e[1;31m[\e[0m\e[1;77m*\e[0m\e[1;31m]\e[0m\e[1;93m Using saved credentials\e[0m\n"
+printf "\e[1;31m[\e[0m\e[1;77m❥\e[0m\e[1;31m]\e[0m\e[1;93m Using saved account\e[0m\n"
 else
 rm -rf cookie.$user
 login_user
@@ -64,7 +70,7 @@ fi
 
 else
 
-read -s -p $'\e[1;31m[\e[0m\e[1;77m*\e[0m\e[1;31m]\e[0m\e[1;93m Password: \e[0m' pass
+read -s -p $'\e[1;31m☛\e[0m\e[1;77m*\e[0m\e[1;31m☜\e[0m\e[1;93m Password: \e[0m' pass
 printf "\n"
 data='{"phone_id":"'$phone'", "_csrftoken":"'$var2'", "username":"'$user'", "guid":"'$guid'", "device_id":"'$device'", "password":"'$pass'", "login_attempt_count":"0"}'
 
@@ -73,7 +79,7 @@ IFS=$'\n'
 hmac=$(echo -n "$data" | openssl dgst -sha256 -hmac "${ig_sig}" | cut -d " " -f2)
 useragent='User-Agent: "Instagram 10.26.0 Android (18/4.3; 320dpi; 720x1280; Xiaomi; HM 1SW; armani; qcom; en_US)"'
 
-printf "\e[1;77m[\e[0m\e[1;92m+\e[0m\e[1;77m] Trying to login as\e[0m\e[1;93m %s\e[0m\n" $user
+printf "\e[1;77m[\e[0m\e[1;92m+\e[0m\e[1;77m] trying to login as\e[0m\e[1;93m %s\e[0m\n" $user
 IFS=$'\n'
 var=$(curl -c cookie.$user -d "ig_sig_key_version=4&signed_body=$hmac.$data" -s --user-agent 'User-Agent: "Instagram 10.26.0 Android (18/4.3; 320dpi; 720x1280; Xiaomi; HM 1SW; armani; qcom; en_US)"' -w "\n%{http_code}\n" -H "$header" "https://i.instagram.com/api/v1/accounts/login/" | grep -o "logged_in_user\|challenge\|many tries\|Please wait" | uniq ); 
 if [[ $var == "challenge" ]]; then printf "\e[1;93m\n[!] Challenge required\n" ; exit 1; elif [[ $var == "logged_in_user" ]]; then printf "\e[1;92m \n[+] Login Successful\n" ; elif [[ $var == "Please wait" ]]; then echo "Please wait"; fi; 
@@ -227,67 +233,13 @@ curl -L -b cookie -d "ig_sig_key_version=4&signed_body=$hmac.$data" -s --user-ag
 }
 
 
-unfollow() {
 
-
-username_id=$(curl -L -s 'https://www.instagram.com/'$user'' > getid && grep -o  'profilePage_[0-9]*.' getid | cut -d "_" -f2 | tr -d '"')
-
-user_id=$(curl -L -s 'https://www.instagram.com/'$user_account'' > getid && grep -o  'profilePage_[0-9]*.' getid | cut -d "_" -f2 | tr -d '"')
-
-data='{"_uuid":"'$guid'", "_uid":"'$username_id'", "user_id":"'$user_id'", "_csrftoken":"'$var2'"}'
-hmac=$(echo -n "$data" | openssl dgst -sha256 -hmac "${ig_sig}" | cut -d " " -f2)
-
-check_unfollow=$(curl -L -b cookie -d "ig_sig_key_version=4&signed_body=$hmac.$data" -s --user-agent 'User-Agent: "Instagram 10.26.0 Android (18/4.3; 320dpi; 720x1280; Xiaomi; HM 1SW; armani; qcom; en_US)"' -w "\n%{http_code}\n" -H "$header" "https://i.instagram.com/api/v1/friendships/destroy/$user_id/" | grep -o '"following": false')
-if [[ $check_unfollow == "" ]]; then
-printf "\e[1;93m [!] Error\n"
-else
-printf "\e[1;92mOK\e[0m\n"
-fi
-}
-
-unfollower() {
-
-user_account=$user
-get_following
-
-printf "\e[1;31m[\e[0m\e[1;77m+\e[0m\e[1;31m]\e[0m\e[1;93m Preparing to unfollow all followers from \e[0m\e[1;77m%s ...\e[0m\n" $user_account
-printf "\e[1;31m[\e[0m\e[1;77m+\e[0m\e[1;31m]\e[0m\e[1;31m Press \"Ctrl + c\" to stop...\e[0m\n"
-sleep 4
-while [[ true ]]; do
-
-
-for unfollow_name in $(cat $user_account.following_backup); do
-
-username_id=$(curl -L -s 'https://www.instagram.com/'$user'' > getmyid && grep -o  'profilePage_[0-9]*.' getmyid | cut -d "_" -f2 | tr -d '"')
-
-user_id=$(curl -L -s 'https://www.instagram.com/'$unfollow_name'' > getunfollowid && grep -o  'profilePage_[0-9]*.' getunfollowid | cut -d "_" -f2 | tr -d '"')
-
-
-data='{"_uuid":"'$guid'", "_uid":"'$username_id'", "user_id":"'$user_id'", "_csrftoken":"'$var2'"}'
-hmac=$(echo -n "$data" | openssl dgst -sha256 -hmac "${ig_sig}" | cut -d " " -f2)
-printf "\e[1;31m[\e[0m\e[1;77m+\e[0m\e[1;31m]\e[0m\e[1;93m Trying to unfollow %s ..." $unfollow_name
-check_unfollow=$(curl -s -L -b cookie.$user -d "ig_sig_key_version=4&signed_body=$hmac.$data" -s --user-agent 'User-Agent: "Instagram 10.26.0 Android (18/4.3; 320dpi; 720x1280; Xiaomi; HM 1SW; armani; qcom; en_US)"' -w "\n%{http_code}\n" -H "$header" "https://i.instagram.com/api/v1/friendships/destroy/$user_id/" | grep -o '"following": false' ) 
-
-if [[ $check_unfollow == "" ]]; then
-printf "\n\e[1;93m [!] Error, stoping to prevent blocking\e[0m\n"
-exit 1
-else
-printf "\e[1;92mOK\e[0m\n"
-fi
-
-sleep 3
-done
-
-
-done
-
-}
 
 increase_followers() {
 
-printf "\e[1;77m[\e[0m\e[1;31m+\e[0m\e[1;77m] This technique consists of following/unfolling celebgrams\e[0m\n"
-printf "\e[1;77m[\e[0m\e[1;31m+\e[0m\e[1;77m] It can increase your followers up to about +30 in 1 hour \e[0m\n"
-printf "\e[1;77m[\e[0m\e[1;31m+\e[0m\e[1;77m]\e[0m\e[1;93m Press Ctrl + C to stop \e[0m\n"
+printf "\e[1;77m[\e[0m\e[1;31m+\e[0m\e[1;77m] This tool used to following/unfollowing users\e[0m\n"
+printf "\e[1;77m[\e[0m\e[1;31m+\e[0m\e[1;77m] It can increase your followers up to about +50 in 1 hour \e[0m\n"
+printf "\e[1;77m[\e[0m\e[1;31m+\e[0m\e[1;77m]\e[0m\e[1;93m Press Ctrl + Z to stop \e[0m\n"
 sleep 5
 
 username_id=$(curl -L -s 'https://www.instagram.com/'$user'' > getid && grep -o  'profilePage_[0-9]*.' getid | cut -d "_" -f2 | tr -d '"')
@@ -303,7 +255,6 @@ basma="20224401"
 najwashihab="48294511"
 trybyl="2273694343"
 qusaihatam="291466354"
-
 maysanber="177841264"
 shimaa_qasim="1782581874"
 alkhafajitv="314195530"
@@ -316,10 +267,41 @@ adil="2540710248"
 raedabufatean="583450031"
 athab="1782327804"
 aloosh="2966187377"
+hoona="3232154008"
+almansory="1606183110"
+ghassan="1493117803"
+sollaf5="1493117803"
+alsharqiyatelevision="6362536337"
+adhamadil="1345062357"
+awosalhlfe="359881048"
+alijasimm="1928048077"
+albasheershow="1928048077"
+husamalrassam="1531662927"
+enaastaleb="5756845644"
+alturky="8505217186"
+fadil="323100909"
+batekhat="2201245028"
+hadii="6007160070"
+raneen="15066705089"
+marry="5659161897"
+dhurgham="1099333694"
+bashar="1358380328"
+alooosh="3155852957"
+shahadalshemari="311258768"
+ahmedalbasheer="398405019"
+aliadnankadhem="398775488"
+alaabbas="6982560780"
+ibrahim="1826684122"
+aseelhameem="1540416231"
+younismahmoud="305610726"
+azawy="2054876108"
+alobaidi="2117640664"
+amerhussen="1301749248"
+algarip="1612902634"
 
 
 if [[ ! -e celeb_id ]]; then
-printf "%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n" $maysanber $mileycyrus $alkhafajitv $star $noorstars $celebrities $kashkoolatheer $mohamedalsalim $adil $raedabufatean $athab $aloosh $nour $mr_doltin $banat_styel $bakr_khald $haifa_hassony $iqiq $saifnabeell $basma $najwashihab $trybyl $qusaihatam > celeb_id
+printf "%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n" $maysanber $mileycyrus $alkhafajitv $star $noorstars $celebrities $kashkoolatheer $mohamedalsalim $adil $raedabufatean $athab $aloosh $nour $mr_doltin $banat_styel $bakr_khald $haifa_hassony $iqiq $saifnabeell $basma $najwashihab $trybyl $qusaihatam $hoona $almansory $ghassan $sollaf5 $alsharqiyatelevision $adhamadil $awosalhlfe $alijasimm $albasheershow $husamalrassam $enaastaleb $alturky $fadil $batekhat $hadii $raneen $marry $dhurgham $bashar $alooosh $shahadalshemari $ahmedalbasheer $aliadnankadhem $alaabbas $ibrahim $aseelhameem $younismahmoud $azawy $alobaidi $amerhussen $algarip > celeb_id
 fi
 
 while [[ true ]]; do
@@ -329,7 +311,7 @@ for celeb in $(cat celeb_id); do
 
 data='{"_uuid":"'$guid'", "_uid":"'$username_id'", "user_id":"'$celeb'", "_csrftoken":"'$var2'"}'
 hmac=$(echo -n "$data" | openssl dgst -sha256 -hmac "${ig_sig}" | cut -d " " -f2)
-printf "\e[1;31m[\e[0m\e[1;77m+\e[0m\e[1;31m]\e[0m\e[1;93m Trying to follow celebgram %s ..." $celeb
+printf "\e[1;31m[\e[0m\e[1;77m+\e[0m\e[1;31m]\e[0m\e[1;93m Trying to follow user %s ..." $celeb
 
 check_follow=$(curl -s -L -b cookie.$user -d "ig_sig_key_version=4&signed_body=$hmac.$data" -s --user-agent 'User-Agent: "Instagram 10.26.0 Android (18/4.3; 320dpi; 720x1280; Xiaomi; HM 1SW; armani; qcom; en_US)"' -w "\n%{http_code}\n" -H "$header" "https://i.instagram.com/api/v1/friendships/create/$celeb/" | grep -o '"following": true')
 
@@ -337,19 +319,19 @@ if [[ $check_follow == "" ]]; then
 printf "\n\e[1;93m [!] Error\n"
 exit 1
 else
-printf "\e[1;92mOK\e[0m\n"
+printf "\e[1;92mGood\e[0m\n"
 fi
 
 sleep 3
 
 done
-printf "\e[1;31m[\e[0m\e[1;77m+\e[0m\e[1;31m]\e[0m\e[1;77m Sleeping 60 secs...\e[0m\n"
-sleep 60
+printf "\e[1;31m[\e[0m\e[1;77m+\e[0m\e[1;31m]\e[0m\e[1;77m Sleeping 300 secs...\e[0m\n"
+sleep 300
 #unfollow
 for celeb in $(cat celeb_id); do
 data='{"_uuid":"'$guid'", "_uid":"'$username_id'", "user_id":"'$celeb'", "_csrftoken":"'$var2'"}'
 hmac=$(echo -n "$data" | openssl dgst -sha256 -hmac "${ig_sig}" | cut -d " " -f2)
-printf "\e[1;31m[\e[0m\e[1;77m+\e[0m\e[1;31m]\e[0m\e[1;93m Trying to unfollow celebgram %s ..." $celeb
+printf "\e[1;31m[\e[0m\e[1;77m+\e[0m\e[1;31m]\e[0m\e[1;93m Trying to unfollow user %s ..." $celeb
 check_unfollow=$(curl -s -L -b cookie.$user -d "ig_sig_key_version=4&signed_body=$hmac.$data" -s --user-agent 'User-Agent: "Instagram 10.26.0 Android (18/4.3; 320dpi; 720x1280; Xiaomi; HM 1SW; armani; qcom; en_US)"' -w "\n%{http_code}\n" -H "$header" "https://i.instagram.com/api/v1/friendships/destroy/$celeb/" | grep -o '"following": false' ) 
 
 if [[ $check_unfollow == "" ]]; then
@@ -361,8 +343,8 @@ fi
 
 sleep 3
 done
-printf "\e[1;31m[\e[0m\e[1;77m+\e[0m\e[1;31m]\e[0m\e[1;77m Sleeping 60 secs...\e[0m\n"
-sleep 60
+printf "\e[1;31m[\e[0m\e[1;77m+\e[0m\e[1;31m]\e[0m\e[1;77m Sleeping 300 secs...\e[0m\n"
+sleep 300
 
 
 done
@@ -382,96 +364,41 @@ curl -L -b cookie.$user -d "ig_sig_key_version=4&signed_body=$hmac.$data" -s --u
 
 
 
-track_unfollowers() {
-
-default_user=$user
-
- 
-read -p $'\e[1;31m[\e[0m\e[1;77m+\e[0m\e[1;31m]\e[0m\e[1;93m Account (leave it blank to use your account): \e[0m' user_account
-
-user_account="${user_account:-${default_user}}"
-
-if [[ -e followers1.$user_account ]]; then
-
-printf "\e[1;31m[\e[0m\e[1;77m+\e[0m\e[1;31m]\e[0m\e[1;93m Last list found for user \e[0m\e[1;77m%s\e[0m\e[1;93m, creating a new and comparing it\e[0m\n" $user_account
-total_followers
-
-cp $user_account.followers_backup followers2.$user_account
-unfollowers=$(grep -Fxv -f followers2.$user_account followers1.$user_account)
-
-
-if [[ $unfollowers != "" ]]; then
-
-printf "\e[1;31m[\e[0m\e[1;77m+\e[0m\e[1;31m]\e[0m\e[1;93m Unfollowers:\e[0m\n"
-
-grep -Fxv -f followers2.$user_account followers1.$user_account >> $user_account.unfollowers
-printf "\e[1;77m\n"
-cat $user_account.unfollowers
-printf "\e[0m\n"
-printf "\e[1;31m[\e[0m\e[1;77m+\e[0m\e[1;31m]\e[0m\e[1;93m Saved: \e[0m\e[1;77m%s.unfollowers\e[0m\n" $user_account
-mv followers2.$user_account followers1.$user_account
-
-else
-printf "\e[1;31m[\e[0m\e[1;77m+\e[0m\e[1;31m]\e[0m\e[1;93m No Unfollower\e[0m\n"
-fi
-
-
-else
-#get  followers
-printf "\e[1;31m[\e[0m\e[1;77m+\e[0m\e[1;31m]\e[0m\e[1;93m Creating followers list\e[0m\n"
-total_followers
-cp $user_account.followers_backup followers1.$user_account
-printf "\e[1;31m[\e[0m\e[1;77m+\e[0m\e[1;31m]\e[0m\e[1;93m Saved!\e[0m\e[1;77m (followers1.%s)\e[0m\n" $user_account
-printf "\e[1;31m[\e[0m\e[1;77m+\e[0m\e[1;31m]\e[0m\e[1;93m Please, run again to track unfollowers\e[0m\n"
-
-fi
-
-
-
-}
-
-
 
 
 menu() {
 
 printf "\n"
-printf " \e[1;31m[\e[0m\e[1;77m01\e[0m\e[1;31m]\e[0m\e[1;93m Unfollow Tracker\e[0m\n"
-printf " \e[1;31m[\e[0m\e[1;77m02\e[0m\e[1;31m]\e[0m\e[1;93m Increase Followers\e[0m\n"
-printf " \e[1;31m[\e[0m\e[1;77m05\e[0m\e[1;31m]\e[0m\e[1;93m Download Following List\e[0m\n"
-printf " \e[1;31m[\e[0m\e[1;77m06\e[0m\e[1;31m]\e[0m\e[1;93m Download Followers List\e[0m\n"
-printf " \e[1;31m[\e[0m\e[1;77m08\e[0m\e[1;31m]\e[0m\e[1;93m Activate Unfollower\e[0m\n"
+printf " \e[1;31m☛\e[0m\e[1;77m01\e[0m\e[1;31m☚\e[0m\e[1;93m get Followers\e[0m\n"
+printf " \e[1;31m☛\e[0m\e[1;77m02\e[0m\e[1;31m☚\e[0m\e[1;93m Download Following List\e[0m\n"
+printf " \e[1;31m☛\e[0m\e[1;77m03\e[0m\e[1;31m☚\e[0m\e[1;93m Download Followers List\e[0m\n"
 printf "\n"
 
 
-read -p $' \e[1;31m[\e[0m\e[1;77m::\e[0m\e[1;31m]\e[0m\e[1;77m Choose an option: \e[0m' option
+read -p $' \e[1;31m[\e[0m\e[1;77m♝\e[0m\e[1;31m]\e[0m\e[1;77m Choose an option: \e[0m' option
 
-if [[ $option -eq 1 ]]; then
-login_user
-track_unfollowers
-
-elif [[ $option -eq 2 ]]; then
+elif [[ $option -eq 1 ]]; then
 login_user
 increase_followers
 
 
 
-elif [[ $option -eq 5 ]]; then
+elif [[ $option -eq 2 ]]; then
 login_user
 default_user=$user
 
  
-read -p $'\e[1;31m[\e[0m\e[1;77m+\e[0m\e[1;31m]\e[0m\e[1;93m Account (leave it blank to use your account): \e[0m' user_account
+read -p $'\e[1;31m[\e[0m\e[1;77m+\e[0m\e[1;31m]\e[0m\e[1;93m Account (Click enter to use your account else Type the username): \e[0m' user_account
 
 user_account="${user_account:-${default_user}}"
 get_following
-elif [[ $option -eq 6 ]]; then
+elif [[ $option -eq 3 ]]; then
 
 login_user
 default_user=$user
 
  
-read -p $'\e[1;31m[\e[0m\e[1;77m+\e[0m\e[1;31m]\e[0m\e[1;93m Account (leave it blank to use your account): \e[0m' user_account
+read -p $'\e[1;31m[\e[0m\e[1;77m+\e[0m\e[1;31m]\e[0m\e[1;93m Account (Click enter to use your account else Type the username): \e[0m' user_account
 
 user_account="${user_account:-${default_user}}"
 total_followers
@@ -479,10 +406,7 @@ total_followers
 
 
 
-elif [[ $option -eq 8 ]]; then
 
-login_user
-unfollower
 
 else
 
